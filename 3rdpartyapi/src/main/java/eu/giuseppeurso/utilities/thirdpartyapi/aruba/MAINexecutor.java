86cd875efe012e15144b68ac2efa4649e28a9925
giuseppeurso-eu <giuseppeurso-eu@github.com>
2016-05-24 12:29:02 +0200

    The first commit for 3rd party api module

---
+++ 3rdpartyapi/src/main/java/eu/giuseppeurso/utilities/thirdpartyapi/aruba/MAINexecutor.java
@@ -0,0 +1,48 @@
+package eu.giuseppeurso.utilities.thirdpartyapi.aruba;
+
+//import it.cloud.computing.api.WsEndUser.IWsEndUser;
+
+public class MAINexecutor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+//	// IWsEndUser.setEnqueueAssociateIpAddress Method (JAVA)
+//	private static void associateIpAddress(IWsEndUser client, ArrayOfint ipAddressResourceIds, int networkAdapterId, boolean setOnVirtualMachine)
+//	{
+//	    //specify the account login details 
+//	    ((BindingProvider) client).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, "ARU-0000");
+//	    ((BindingProvider) client).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, "1234567890");
+//
+//	    try
+//	    {
+//	        // create the object IpAddressesRequest for the request 
+//	        // with data to process
+//	        IpAddressesRequest request = new IpAddressesRequest();
+//	        request.setIpAddressResourceIds(ipAddressResourceIds);
+//	        request.setNetworkAdapterId(networkAdapterId);
+//	        request.setSetOnVirtualMachine(setOnVirtualMachine);
+//
+//	        //call method setEnqueueAssociateIpAddress, 
+//	        //obtaining a WsResult item
+//	        WsResult result = client.setEnqueueAssociateIpAddress(request);
+//
+//	        //if the call is not successful, re-run the error indicating the message
+//	        if (!result.isSuccess())
+//	            throw new Exception(result.getResultMessage().getValue());
+//	    }
+//	    catch (Exception ex)
+//	    {
+//	        //show error
+//	        System.out.println(ex);
+//	    }
+//	}
+	
+	
+
+}
