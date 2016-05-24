f68f6d4859764c1204333a48f45a4154e27406d9
giuseppeurso-eu <giuseppeurso-eu@github.com>
2016-05-24 16:48:31 +0200

    Random numbers as string

--- generics/src/main/java/eu/giuseppeurso/utilities/generics/math/Calculator.java
+++ generics/src/main/java/eu/giuseppeurso/utilities/generics/math/Calculator.java
@@ -1,5 +1,7 @@
 package eu.giuseppeurso.utilities.generics.math;
 
+import java.util.Random;
+
 /**
  * Mathematics
  * @author Giuseppe Urso - www.giuseppeuros.eu
@@ -8,4 +10,25 @@ package eu.giuseppeurso.utilities.generics.math;
 public class Calculator {
 
 	
+	public static void main(String argv[]) {
+		randomNumberAsString("3", "10", 5);
+	}
+
+/**
+ * Random integer from string
+ * @param min
+ * @param max
+ */
+static void randomNumberAsString(String min, String max, int total ){
+	System.out.println("Range of numbers: ["+min+","+max+"]");
+	System.out.println("Total of numbers: "+total);
+	int mn = Integer.parseInt(min);
+	int mx = Integer.parseInt(max);
+	for (int i = 0; i < total; i++) {
+		Random random = new Random();
+		int randomNum = random.nextInt((mx - mn) + 1) + mn;
+		System.out.println("Random String Number: "+ String.valueOf(randomNum));
+	}
+}
+
 }
