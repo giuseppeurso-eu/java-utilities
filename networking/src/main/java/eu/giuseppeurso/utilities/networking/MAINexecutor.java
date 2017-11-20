700ef6492f27093d1a140ce55d4d4125ddc161ba
giuseppeurso-eu <urso.giu@gmail.com>
2017-11-20 17:12:33 +0100

    First commit for the BlackHour checker

--- networking/src/main/java/eu/giuseppeurso/utilities/networking/MAINexecutor.java
+++ networking/src/main/java/eu/giuseppeurso/utilities/networking/MAINexecutor.java
@@ -1,5 +1,6 @@
 package eu.giuseppeurso.utilities.networking;
 
+
 public class MAINexecutor {
 
 	public static void main(String[] args) {
@@ -10,6 +11,10 @@ public class MAINexecutor {
 		//NtlmHttpClient.connectWithJCIFS("TESTAD", "paolo", "1234", "http://localhost:8080/share");
 		//NtlmHttpClient.connectWithAPACHEHttpComponent42("TESTAD", "paolo", "gius-pc", "1234", "http://localhost:8080/share", "localhost",8080);
 		//NtlmHttpClient.connectWithAPACHEHttpComponent43("TESTAD", "paolo", "gius-pc", "1234", "http://localhost:8080/share", "localhost",8080);
+		
+		
+		// Send Email Message
+		Email.sendMailTLS("user@gmail.com", "1234", true, true, "smtp.gmail.com", "587", "mawdev@gmail.com", "dest@eee.it", "notification", "This is a notification test");
 	}
 
 }
