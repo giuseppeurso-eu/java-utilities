94b71c91c01cbf85465f21ef7f2ce483e635de73
giuseppeurso-eu <urso.giu@gmail.com>
2017-11-20 17:07:50 +0100

    getPojoGettersValues() added

--- generics/src/main/java/eu/giuseppeurso/utilities/generics/strings/StringPlayer.java
+++ generics/src/main/java/eu/giuseppeurso/utilities/generics/strings/StringPlayer.java
@@ -1,5 +1,12 @@
 package eu.giuseppeurso.utilities.generics.strings;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
 public class StringPlayer {
 	
 	
@@ -21,5 +28,41 @@ public class StringPlayer {
 		}else {
 			return null;
 		}
+	}
+	
+	/**
+	 * Generate random UUID
+	 * @return
+	 * @see http://docs.oracle.com/javase/7/docs/api/java/util/UUID.html#randomUUID%28%29
+	 */
+	public static String getRandomUUID() {
+		int length = 10;
+		boolean useLetters = true;
+		boolean useNumbers = false;
+		UUID id = UUID.randomUUID();
+		System.out.println(id);
+		return id.toString();
+	}
+	
+	/**
+	 * @param <T>
+	 *
+	 */
+	public static Map<String, String> getPojoGettersValues(Object pojo) {
+		Map<String, String> gettersValues = new HashMap<String, String>();
+		for (Method m : pojo.getClass().getMethods())
+		    if (m.getName().startsWith("get") && m.getParameterTypes().length == 0) {
+		      try {
+				Object r = m.invoke(pojo);
+				System.out.println(m.getName()+": "+r+" "+r.getClass().getTypeName());
+		      } catch (IllegalAccessException e) {
+					e.printStackTrace();
+		      } catch (IllegalArgumentException e) {
+					e.printStackTrace();
+		      } catch (InvocationTargetException e) {
+					e.printStackTrace();
+		      }
+		    }
+		return gettersValues;
 	}
 }
