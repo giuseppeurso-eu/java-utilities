c52732b5e1fdfc683a8040ff0cfa0f0392ba8a6b
giuseppeurso-eu <urso.giu@gmail.com>
2018-06-05 16:27:21 +0200

    Playing with RNG

--- generics/src/main/java/eu/giuseppeurso/utilities/generics/math/Calculator.java
+++ generics/src/main/java/eu/giuseppeurso/utilities/generics/math/Calculator.java
@@ -3,7 +3,11 @@ package eu.giuseppeurso.utilities.generics.math;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
+
+import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
 
 
 
@@ -14,85 +18,119 @@ import java.util.Random;
  */
 public class Calculator {
 	
-/**
- * Random integer from string
- * @param min
- * @param max
- */
-static void randomNumberAsString(String min, String max, int total ){
-	System.out.println("Range of numbers: ["+min+","+max+"]");
-	System.out.println("Total of numbers: "+total);
-	int mn = Integer.parseInt(min);
-	int mx = Integer.parseInt(max);
-	for (int i = 0; i < total; i++) {
-		Random random = new Random();
-		int randomNum = random.nextInt((mx - mn) + 1) + mn;
-		System.out.println("Random String Number: "+ String.valueOf(randomNum));
-	}
-}
-
-/**
- * Verifies if couples of integers are both greater than specified limits or not.
- * 
- * @param sideA
- * @param sideB
- * @param width limit 
- * @param height limit
- * @return
- */
-static Boolean twoNumberGreaterThanLimits(int sideA, int sideB, int width, int height){
-	System.out.println("Comparing "+sideA+" with limit "+width);
-	System.out.println("Comparing "+sideB+" with limit "+height);
-    return !(sideA < width && sideB < height);
-}
-
-/**
- * Get random integers in a given range.
- * @param min
- * @param max
- * @param totalNumbersToPrint
- */
-static void printRandomNumbers(int min, int max, int totalNumbersToPrint ){
-	System.out.println("Range of numbers: ["+min+","+max+"]");
-	for (int i = 0; i < totalNumbersToPrint; i++) {
-		Random random = new Random();
-		int randomNum = random.nextInt((max - min) + 1) + min;
-		System.out.println("Random Number: "+ randomNum);
-	}
-}
-
-static int[] getRandomNumbers(int min, int max, int totalNumbersToGet ){
-	int[] numbers = new int[totalNumbersToGet];
-	System.out.println("Range of numbers: ["+min+","+max+"]");
-	for (int i = 0; i < totalNumbersToGet; i++) {
-		Random random = new Random();
-		int randomNum = random.nextInt((max - min) + 1) + min;
-		System.out.println("Generated random Number: "+ randomNum);
-		// For uniqueness, check if the set to be returned contains the number generates each time
-		for (int j = 0; j < numbers.length; j++) {
-			System.out.println(j+") Current array number:"+numbers[j]);
-			if (numbers[j]==randomNum) {
-				System.out.println("Collision avoided for number: "+numbers[j]+"<>"+randomNum);
-				randomNum = random.nextInt((max - min) + 1) + min;
-			}
+	/**
+	 * Random integer from string
+	 * @param min
+	 * @param max
+	 */
+	static void randomNumberAsString(String min, String max, int total ){
+		System.out.println("Range of numbers: ["+min+","+max+"]");
+		System.out.println("Total of numbers: "+total);
+		int mn = Integer.parseInt(min);
+		int mx = Integer.parseInt(max);
+		for (int i = 0; i < total; i++) {
+			Random random = new Random();
+			int randomNum = random.nextInt((mx - mn) + 1) + mn;
+			System.out.println("Random String Number: "+ String.valueOf(randomNum));
 		}
-		numbers[i] = randomNum;
-		System.out.println("Saved number: "+numbers[i]);
+	}
+	
+	/**
+	 * Verifies if couples of integers are both greater than specified limits or not.
+	 * 
+	 * @param sideA
+	 * @param sideB
+	 * @param width limit 
+	 * @param height limit
+	 * @return
+	 */
+	static Boolean twoNumberGreaterThanLimits(int sideA, int sideB, int width, int height){
+		System.out.println("Comparing "+sideA+" with limit "+width);
+		System.out.println("Comparing "+sideB+" with limit "+height);
+	    return !(sideA < width && sideB < height);
+	}
+	
+	/**
+	 * Get random integers in a given range.
+	 * @param min
+	 * @param max
+	 * @param totalNumbersToPrint
+	 */
+	static void printRandomNumbers(int min, int max, int totalNumbersToPrint ){
+		System.out.println("Range of numbers: ["+min+","+max+"]");
+		for (int i = 0; i < totalNumbersToPrint; i++) {
+			Random random = new Random();
+			int randomNum = random.nextInt((max - min) + 1) + min;
+			System.out.println("Random Number: "+ randomNum);
+		}
+	}
+	
+	static int[] getRandomNumbers(int min, int max, int totalNumbersToGet ){
+		int[] numbers = new int[totalNumbersToGet];
+		System.out.println("Range of numbers: ["+min+","+max+"]");
+		for (int i = 0; i < totalNumbersToGet; i++) {
+			Random random = new Random();
+			int randomNum = random.nextInt((max - min) + 1) + min;
+			System.out.println("Generated random Number: "+ randomNum);
+			// For uniqueness, check if the set to be returned contains the number generates each time
+			for (int j = 0; j < numbers.length; j++) {
+				System.out.println(j+") Current array number:"+numbers[j]);
+				if (numbers[j]==randomNum) {
+					System.out.println("Collision avoided for number: "+numbers[j]+"<>"+randomNum);
+					randomNum = random.nextInt((max - min) + 1) + min;
+				}
+			}
+			numbers[i] = randomNum;
+			System.out.println("Saved number: "+numbers[i]);
+			
+		}
+		return numbers;
+	}
+	
+	/**
+	 * Generates a list of random numbers in a range
+	 */
+	static List<Integer> getRandomNumberList(int min, int max, int totalToGet){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+	    for (int i=min; i<max; i++) {
+	        list.add(new Integer(i));
+	    }
+	    Collections.shuffle(list);
+	    ArrayList<Integer> listWithRandomNumbers = new ArrayList<Integer>();
+	    for (int i=0; i<totalToGet; i++) {
+	    	listWithRandomNumbers.add(list.get(i));
+	    }
+	    return listWithRandomNumbers;
+	}
+	
+	/**
+	 * Generating random integers within range with a probability distribution using Apache Math EnumeratedIntegerDistribution
+	 * @param sequence
+	 * @param sampleToExtract
+	 * @see http://commons.apache.org/proper/commons-math/javadocs/api-3.6/org/apache/commons/math3/distribution/EnumeratedDistribution.html
+	 */
+	static void randomNumbersWithProbabilityDistribution(Map<Integer, Double> sequence, int sampleToExtract){
+		// Creating two arrays for numbers and probabilities
+		int[] numbers = new int[sequence.size()];
+		double[] probabilities = new double[sequence.size()];
+		int i = 0; 
+		for (Entry<Integer, Double> entry : sequence.entrySet()) {
+			double probDouble = entry.getValue()/100.0;
+			System.out.println("Number: " + entry.getKey() + " Probability: " + entry.getValue()+"% (double "+probDouble+")");
+			numbers[i]=entry.getKey();
+			probabilities[i]=probDouble;
+			System.out.println();
+			i++;
+		}
+		// Creating the number/probabilities distribution
+		EnumeratedIntegerDistribution distribution = new EnumeratedIntegerDistribution(numbers, probabilities);
+
+		// Extracting numbers
+		int[] samples = distribution.sample(sampleToExtract);
+		for (int j = 0; j < samples.length; j++) {
+			System.out.println(samples[j]);
+		}
+		
 		
 	}
-	return numbers;
-}
-
-static List<Integer> getRandomNumberList(int min, int max, int totalToGet){
-	ArrayList<Integer> list = new ArrayList<Integer>();
-    for (int i=min; i<max; i++) {
-        list.add(new Integer(i));
-    }
-    Collections.shuffle(list);
-    ArrayList<Integer> listWithRandomNumbers = new ArrayList<Integer>();
-    for (int i=0; i<totalToGet; i++) {
-    	listWithRandomNumbers.add(list.get(i));
-    }
-    return listWithRandomNumbers;
-}
 }
--- generics/src/main/java/eu/giuseppeurso/utilities/generics/math/MAINexecutor.java
+++ generics/src/main/java/eu/giuseppeurso/utilities/generics/math/MAINexecutor.java
@@ -2,16 +2,16 @@ package eu.giuseppeurso.utilities.generics.math;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MAINexecutor {
 	
 	public static void main(String argv[]) {
 		
-		//Calculator - randomNumberAsString()
-		//randomNumberAsString("3", "10", 5);
+//		Calculator.randomNumberAsString("1", "91", 1);
 		
-		//Calculator - isGreaterThan()
 //		int sideA=20;
 //		int sideB=10;
 //		int limitA=30;
@@ -22,9 +22,22 @@ public class MAINexecutor {
 //			System.out.println("Both given numbers are not greater than limits specified!");
 //		}
 		
-		List<Integer> listNum = Calculator.getRandomNumberList(1, 7, 5);
-		for (int i = 0; i < listNum.size(); i++) {
-			System.out.println("Num: "+listNum.get(i));
+//		List<Integer> listNum = Calculator.getRandomNumberList(1, 7, 5);
+//		for (int i = 0; i < listNum.size(); i++) {
+//			System.out.println("Num: "+listNum.get(i));
+//		}
+		
+		
+//		Map<Integer, Double> seq = new HashMap<>();
+//		seq.put(1, 70.0);
+//		seq.put(2, 9.5);
+//		seq.put(3, 10.0);
+//		seq.put(4, 70.0);
+//		seq.put(5, 10.0);
+//		Calculator.randomNumbersWithProbabilityDistribution(seq,10);
+		
+		for (int i = 1; i <=10 ; i++) {
+			System.out.println(i);
 		}
 		
 	}
